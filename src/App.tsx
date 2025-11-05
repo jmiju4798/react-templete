86c59fa45ab9ac3d96db798737cd73469f96d994
jeongmiju <jmiju4644@etribe.co.kr>
2025-11-05 10:54:40 +0900

    소스 업데이트

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CodingListTable from "./features/codingList/components/CodingListTable";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <CodingListTable />
+    </div>
+  );
+}
+
+export default App;
